2c91eb5f809dfac95ac4c08d50d8b49553622755
sergio miranda <sergiomiranda@sergios-MacBook-Pro.local>
2022-08-09 20:41:47 -0400

    desafio listo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row mt-5">
+        <Formulario
+          labelName={"Nombre:"}
+          labelPass={"Password:"}
+          estadoNombre={""}
+          estadoPass={""}
+        />
+      </div>
+    </>
   );
 }
 
